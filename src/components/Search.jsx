13b2fc635803b00a3ceacbc342b2b4c9a294c01d
Tilio77 <tiliobolso1@gmail.com>
2022-10-31 21:10:08 -0300

    Debounced search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,5 @@
 import styles from './Search.module.css';
 import { FaSearch } from 'react-icons/fa';
-import { useState, useEffect } from 'react';
 import { useHistory } from "react-router-use-history";
 import { useQuery } from '../hooks/useQuery';
 
@@ -9,16 +8,10 @@ export function Search() {
 	const query = useQuery();
 	const search = query.get("search");
 
-	const [searchText, setSearchText] = useState("");
 	const history = useHistory();
-
-	useEffect(() => {
-		setSearchText(search || "");
-	}, [search]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		history.push("/?search=" + searchText);
 	}
 
 	return(
@@ -26,8 +19,12 @@ export function Search() {
 			<div className={styles.searchBox}>
 				<input 
 					className={styles.searchInput} 
-					type="text" value={searchText} 
-					onChange={(e) => setSearchText(e.target.value)} 
+					type="text" 
+					value={search} 
+					onChange={(e) => {
+						const value = (e.target.value);
+						history.push("/?search=" + value);
+					}}
 				/>
 				<button className={styles.searchButton} type="submit">
 					<FaSearch size={20} />
